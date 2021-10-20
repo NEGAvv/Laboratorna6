58638f76e10c9ddb5d178af3ef04cd498bd26d47
NEGAvv <hudolija@gmail.com>
2021-10-20 20:25:33 +0300

    First try

---
+++ laba 5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double a, x, b, c;
+	float F;
+	cout << "Input X" << endl;
+	cin >> x;
+	cout << "Input A" << endl;
+	cin >> a;
+	cout << "Input B" << endl;
+	cin >> b;
+	cout << "Input C" << endl;
+	cin >> c;
+	if (((x + 5) < 0) && (c == 0))
+	{
+		if ((a == 0) || (x == 0)) {
+			F = false;
+		}
+		else {
+			F = (1 / (a * x)) - b;
+		}
+		
+	}
+	else if ((x + 5 > 0) && (c != 0)) {
+		if (x == 0) {
+			F = false;
+		}
+		else {
+			F = (x - a) / x;
+		}	
+	}
+	else
+	{
+		if (c == 4) {
+			F = false;
+		}
+		else {
+			F = 10 * x / (c - 4);
+		}
+	}
+	if (F == false) {
+		cout << "error\n";
+	}
+	else {
+		cout << "Result F = " << F << endl;
+	}
+}
