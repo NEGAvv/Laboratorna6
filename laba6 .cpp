37e6738d80bf7aace1ffc18b9ca4575ef8719805
Alina Khudolii <hudolija@gmail.com>
2021-10-28 16:14:16 +0300

    NewFile

---
+++ laba6 .cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <iomanip>
+using namespace std;
+int main()
+{
+	float minX = 1.5, maxX = 2.5, h = 0.1, x;
+	double y;
+	cout << "Input minX=1.5" << endl;
+	cout << "Input maxX=2.5" << endl;
+	cout << "Input h=0.1" << endl;
+	x = minX;
+	while (x <= maxX + h / 2)
+	{
+		y = (x * x * x) + 3 * sin(x * x) - 3;
+		cout << setw(4) << x << setw(10) << y << endl;
+		x += h;
+	}
+}
